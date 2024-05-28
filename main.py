2f9fd4cb0b08829cdf73732ee6e5eb581ab61479
Ukso <oskari.vierijarvi@gmail.com>
2024-05-28 23:38:20 +0300

    added count letters

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 def main():
     file = ("books/frankenstein.txt")
     with open(file) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        word_count = len(words)
-    print (f"Words in book: {word_count}")
-        
+        text = f.read()
+        words = word_count (text)
+        letter_count = count_letters (text)
+    print (f"Words in book: {words}")
+    print (f"letters in book: {letter_count}")
+
+def word_count (text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text):
+    lowered_text = text.lower()
+    letters_dict = {}
+    for element in lowered_text:
+        if element.isalpha():
+            if element not in letters_dict:
+              print(element)
+              print("false")
+              letters_dict[element] = 1
+            calculator = letters_dict[element]
+            calculator= calculator + 1
+            letters_dict[element] = calculator
+    return letters_dict
 
 main()
